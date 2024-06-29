e6ac5fa19e02f15c1d8c17b896d312fa643b6b2f
mehedihasan2061 <mehedi.hasan206191@gmail.com>
2024-06-29 21:44:38 +0600

    added

---
+++ index.js
@@ -0,0 +1,143 @@
+const express = require("express");
+require('dotenv').config()
+const cors=require('cors')
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// middleware 
+app.use(cors());
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send("terrific Travels");
+});
+
+// phL7d5GGlUIRqZhw;
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.daifw6e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+     
+    const touristSpotCollection = client.db("touristSpotDB").collection("spotUser")
+    const placeCollection = client.db("touristSpotDB").collection("country");
+    
+    app.get('/spot', async (req, res) => {
+      const cursor = touristSpotCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.get('/spot/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) };
+      const result = await touristSpotCollection.findOne(query)
+      res.send(result)
+    })
+    
+
+    app.get('/mySpot/:email', async (req, res) => {
+      console.log(req.params.email); 
+      const result = await touristSpotCollection
+        .find({ email: req.params.email })
+        .toArray();
+      res.send(result);
+      
+      
+    })
+
+   
+
+    app.post('/spot', async (req, res) => {
+      const user = req.body;
+      // console.log(user);
+      const result = await touristSpotCollection.insertOne(user)
+      res.send(result)
+    })
+
+   
+
+  
+  app.put("/spot/:id", async (req, res) => {
+    const id = req.params.id;
+    const spot = req.body;
+    console.log(id, spot);
+    const filter = { _id: new ObjectId(id) };
+    const options = { upsert: true };
+    const updateItem = {
+      $set: {
+        photo: spot.photo,
+        touristsSpotName: spot.touristsSpotName,
+        country_name: spot.country_name,
+        location: spot.location,
+        short_description: spot.short_description,
+        average_cost: spot.average_cost,
+        seasonality: spot.seasonality,
+        travel_time: spot.travel_time,
+        totalVisitorsPerYear: spot.totalVisitorsPerYear,
+        email: spot.email,
+        name:spot.name
+
+      },
+    };
+    const result = await touristSpotCollection.updateOne(filter, updateItem, options)
+    res.send(result)
+  });
+    
+    app.delete('/mySpot/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) }
+      const result = await touristSpotCollection.deleteOne(query)
+      res.send(result)
+    })
+
+
+
+    // country data 
+     app.get("/country", async (req, res) => {
+       const cursor = placeCollection.find();
+       const result = await cursor.toArray();
+       res.send(result);
+     });
+    
+     app.post("/country", async (req, res) => {
+       const user = req.body;
+       console.log(user);
+       const result = await placeCollection.insertOne(user);
+       res.send(result);
+     });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+  console.log(`Terrific travels is running on port ${port}`);
+});
